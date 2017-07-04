395bed66b894fd3f2a521fb9e6d1fafa13a65344
Gergely Papi <gergely.papi.cc@gmail.com>
2017-07-04 09:54:23 +0200

    added joke function

--- gittie.py
+++ gittie.py
@@ -1,1 +1,5 @@
 print("Hello, I'm Gittie!")
+
+
+def joke():
+    print( "My girl, Ginger, is going to die of syphilis, mumbles an angry biker to one of his buddies. No, says the friend, people don't die of syphilis anymore. The angry biker replies,They do when they give it to me!" )
