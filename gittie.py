4dddeb073c657e23731a4a3f76c571b00229d9d1
Gergely Papi <gergely.papi.cc@gmail.com>
2017-07-04 10:00:34 +0200

    modified intro func

--- gittie.py
+++ gittie.py
@@ -1,10 +1,10 @@
-print("Hello, I'm Gittie!")
+def introduce():
+    print("Hello, I'm Gittie!")
+    print("It is random day")
 
 
-<<<<<<< HEAD
 def joke():
     print( "My girl, Ginger, is going to die of syphilis, mumbles an angry biker to one of his buddies. No, says the friend, people don't die of syphilis anymore. The angry biker replies,They do when they give it to me!" )
-=======
 def add(a,b):
     return (a+b)
->>>>>>> 805cc75ea8964d4169c2eef9f53e93aa8b48e996
+
